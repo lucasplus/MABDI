cedbc81ad904d51463391655eab2c8a6162d58d6
Lucas Chavez <golucasplus@gmail.com>
2016-01-11 15:32:59 -0700

    Separating concept of pov and depth
    There is a vtkRenderer and a vtkRenderWindow that have to be created for
    the concept of the pov of the sensor and also for the concept of the
    depth. So use a c-like struct to make that clear

--- mabdi/_SimulateSensor.py
+++ mabdi/_SimulateSensor.py
@@ -11,61 +11,62 @@ class _SimulateSensor(object):
     def __init__(self):
         # initialize vtk objects
 
+        # class to act as a c struct and contain a vtkRenderer and vtkRenderWindow
+        class _VTK_Renderer_And_RenderWindow:
+            renderer = vtk.vtkRenderer()
+            window = vtk.vtkRenderWindow()
+
         # Renderer and RenderWindow rendering the POV of the sensor
-        self.renderer = vtk.vtkRenderer()
-        self.ren_win = vtk.vtkRenderWindow()
-        self.ren_win.AddRenderer(self.renderer)
-        # configure 
-        self.renderer.SetBackground(0.1, 0.2, 0.4)
-        self.ren_win.SetSize(640, 480)
-        self.ren_win.Start() # works without it
+        self._pov = _VTK_Renderer_And_RenderWindow()
+        self._pov.window.AddRenderer(self._pov.renderer)
+        self._pov.window.SetSize(640, 480)
+        self._pov.renderer.SetBackground(0.1, 0.2, 0.4)
+        self.renderer = self._pov.renderer
 
         # Renderer and RenderWindow for an image created by a vtkWindowToImageFilter
         # that gets the depth from the render window
-        self.d_renderer = vtk.vtkRenderer()
-        self.d_ren_win = vtk.vtkRenderWindow()
-        self.d_ren_win.AddRenderer(self.d_renderer)
-        self.d_ren_win.SetSize(640, 480)
+        self._depth = _VTK_Renderer_And_RenderWindow()
+        self._depth.window.AddRenderer(self._depth.renderer)
+        self._depth.window.SetSize(640, 480)
         
         # filter that grabs the vtkRenderWindow and returns 
         # the depth image (in this case)
         self.filter = vtk.vtkWindowToImageFilter()
         self.filter.SetInputBufferTypeToZBuffer()
-        self.filter.SetInput( self.renderer.GetVTKWindow() )
+        self.filter.SetInput( self._pov.renderer.GetVTKWindow() )
         self.filter.Update()
         
         # take the output of the mapper and turn it into an actor that
         # can be rendered
-        self.image_mapper = vtk.vtkImageMapper()
-        self.image_mapper.SetInputConnection( self.filter.GetOutputPort() )
-        self.image_mapper.SetColorWindow( 1.0 );
-        self.image_mapper.SetColorLevel( 0.5 );
-        self.image_actor = vtk.vtkActor2D()
-        self.image_actor.SetMapper( self.image_mapper )
-        self.d_renderer.AddActor( self.image_actor )
+        image_mapper = vtk.vtkImageMapper()
+        image_mapper.SetInputConnection( self.filter.GetOutputPort() )
+        image_mapper.SetColorWindow( 1.0 );
+        image_mapper.SetColorLevel( 0.5 );
+        image_actor = vtk.vtkActor2D()
+        image_actor.SetMapper( image_mapper )
+        self._depth.renderer.AddActor( image_actor )
 
         # add observer
-        self.ren_win.AddObserver( 'RenderEvent' , self._callback_get_depth_image ) 
+        self._pov.window.AddObserver( 'RenderEvent' , self._callback_get_depth_image ) 
 
         # set camera intrinsic parameters 
-        self.renderer.GetActiveCamera().SetViewAngle( 60.0 );
-        self.renderer.GetActiveCamera().SetClippingRange( 0.5, 10.0 );
+        self._pov.renderer.GetActiveCamera().SetViewAngle( 60.0 );
+        self._pov.renderer.GetActiveCamera().SetClippingRange( 0.5, 10.0 );
 
     def _callback_get_depth_image( self, obj, env ):
-        print("I rendered")
         self.filter.Modified()
-        self.d_ren_win.Render()
+        self._depth.window.Render()
 
     def move_camera( self, in_position, in_lookat ):
         # TODO: make sure in_position and in_lookat are the same size
         # TODO: make sure arguments are of size Nx3
 
-        camera = self.renderer.GetActiveCamera()
+        camera = self._pov.renderer.GetActiveCamera()
 
         for pos,lka in zip(in_position,in_lookat):
             camera.SetPosition( pos )
             camera.SetFocalPoint( lka )
-            self.ren_win.Render()
+            self._pov.window.Render()
             time.sleep(0.1)
 
 
