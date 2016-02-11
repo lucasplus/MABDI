b3848eae55804511671764797ee2b9eb5800c233
Lucas Chavez <golucasplus@gmail.com>
2016-02-11 12:18:59 -0700

    Rearrange so functions are top and cut out not needed things

--- scripts/ScratchProjectAllPoints.py
+++ scripts/ScratchProjectAllPoints.py
@@ -11,67 +11,6 @@ import matplotlib.pyplot as plt
 from timeit import default_timer as timer
 import time
 
-# ren, renWin, iren - vtk rendering objects
-ren = vtk.vtkRenderer()
-renWin = vtk.vtkRenderWindow()
-renWin.AddRenderer(ren)
-iren = vtk.vtkRenderWindowInteractor()
-iren.SetRenderWindow(renWin)
-
-# create cube
-cube = vtk.vtkCubeSource()
-cube.SetCenter(0, 0, 0)
-cubeMapper = vtk.vtkPolyDataMapper()
-cubeMapper.SetInputConnection(cube.GetOutputPort())
-cubeActor = vtk.vtkActor()
-cubeActor.SetMapper(cubeMapper)
-ren.AddActor(cubeActor)
-
-# world point picker
-picker = vtk.vtkWorldPointPicker()
-iren.SetPicker(picker)
-
-# set camera intrinsic params to mimic kinect
-ren.GetActiveCamera().SetViewAngle(60.0)
-ren.GetActiveCamera().SetClippingRange(0.1, 10.0)
-iren.GetInteractorStyle().SetAutoAdjustCameraClippingRange(0)
-
-# vtk objects for the point cloud
-points = vtk.vtkPoints()
-vertices = vtk.vtkCellArray()
-polydata = vtk.vtkPolyData()
-"""
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputData(polydata)
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-actor.GetProperty().SetPointSize(2)
-rgb = [0.0, 0.0, 0.0]
-colors = vtk.vtkNamedColors()
-colors.GetColorRGB("red", rgb)
-actor.GetProperty().SetColor(rgb)
-
-ren.AddActor(actor)
-"""
-
-iren.Initialize()
-renWin.Render()
-renWin.Render()
-
-# depth_image_filter that grabs the vtkRenderWindow and returns 
-# the depth image (in this case)
-depth_image_filter = vtk.vtkWindowToImageFilter()
-depth_image_filter.SetInputBufferTypeToZBuffer()
-depth_image_filter.SetInput(ren.GetVTKWindow())
-depth_image_filter.Update()
-
-dfilter = dsa.WrapDataObject(depth_image_filter.GetOutput())
-image = numpy_support.vtk_to_numpy(dfilter.PointData['ImageScalars']).reshape(300, 300)
-dfig = plt.imshow(image)
-plt.colorbar()
-time.sleep(.01)
-plt.show()
-
 
 def project_pixel(d_x, d_y, d_z):
 
@@ -96,10 +35,71 @@ def project_pixel(d_x, d_y, d_z):
 def render_point_cloud(obj, env):
     start = timer()
 
+    ren.Render()
+    depth_image_filter.Update()
+    depth_image_filter.Modified()
+
     (sizex, sizey) = obj.GetSize()
     d_pts = np.ones((4, sizex*sizey))
 
-    """
+    dfilter = dsa.WrapDataObject(depth_image_filter.GetOutput())
+    image = numpy_support.vtk_to_numpy(dfilter.PointData['ImageScalars']).reshape((sizey, sizex))
+    plt.imshow(image, origin='lower')
+    plt.colorbar()
+    plt.show()
+
+    end = timer()
+    print(end-start)
+
+
+# ren, renWin, iren - vtk rendering objects
+ren = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+# create cube
+cube = vtk.vtkCubeSource()
+cube.SetCenter(0, 0, 0)
+cubeMapper = vtk.vtkPolyDataMapper()
+cubeMapper.SetInputConnection(cube.GetOutputPort())
+cubeActor = vtk.vtkActor()
+cubeActor.SetMapper(cubeMapper)
+ren.AddActor(cubeActor)
+
+# set camera intrinsic params to mimic kinect
+ren.GetActiveCamera().SetViewAngle(60.0)
+ren.GetActiveCamera().SetClippingRange(0.1, 10.0)
+iren.GetInteractorStyle().SetAutoAdjustCameraClippingRange(0)
+ren.GetActiveCamera().SetPosition(0.0, 0.0, 2.0)
+
+# vtk objects for the point cloud
+points = vtk.vtkPoints()
+vertices = vtk.vtkCellArray()
+polydata = vtk.vtkPolyData()
+
+# has to be initialized before filter is update
+# not sure why
+iren.Initialize()
+
+# depth_image_filter that grabs the vtkRenderWindow and returns 
+# the depth image (in this case)
+depth_image_filter = vtk.vtkWindowToImageFilter()
+depth_image_filter.SetInputBufferTypeToZBuffer()
+depth_image_filter.SetInput(ren.GetVTKWindow())
+depth_image_filter.Update()
+
+# needed for first image in loop to be current
+ren.Render()
+depth_image_filter.Update()
+depth_image_filter.Modified()
+
+iren.AddObserver('UserEvent', render_point_cloud)
+
+iren.Start()
+
+"""
     points.Reset()
     vertices.Reset()
     for x in np.arange(obj.GetSize()[0]):
@@ -113,23 +113,18 @@ def render_point_cloud(obj, env):
     polydata.SetVerts(vertices)
     polydata.Modified()
     mapper.Update()
-    """
+"""
 
-    iren.Render()
-    depth_image_filter.Update()
-    depth_image_filter.Modified()
+"""
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputData(polydata)
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+actor.GetProperty().SetPointSize(2)
+rgb = [0.0, 0.0, 0.0]
+colors = vtk.vtkNamedColors()
+colors.GetColorRGB("red", rgb)
+actor.GetProperty().SetColor(rgb)
 
-    dfilter = dsa.WrapDataObject(depth_image_filter.GetOutput())
-    image = numpy_support.vtk_to_numpy(dfilter.PointData['ImageScalars']).reshape((sizey, sizex))
-    plt.imshow(image, origin='lower')
-    plt.colorbar()
-    plt.show()
-
-    end = timer()
-    print(end-start)
-
-iren.AddObserver('UserEvent', render_point_cloud)
-
-# enable user interface interactor
-
-iren.Start()
+ren.AddActor(actor)
+"""
