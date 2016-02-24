73006aa621b32160fc3adf19ba51bc5558a841ba
Lucas Chavez <golucasplus@gmail.com>
2016-02-24 16:36:44 -0700

    Creating my own depth image filter to replace vtkWindowToImageFilter - removed class I had for debugging

--- mabdi/__init__.py
+++ mabdi/__init__.py
@@ -1,6 +1,5 @@
 # from SimulateScenario import SimulateScenario
 
 from DepthImageFilter import DepthImageFilter
-from DepthImageFilter import ImagePassthroughFilter
 
 from MabdiUtilities import VTKRenderObjects
