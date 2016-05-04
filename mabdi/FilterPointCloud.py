b43d2507c31944df32eca090075bad34660d39c9
Lucas Chavez <golucasplus@gmail.com>
2016-05-04 17:09:38 -0600

    FilterPointCloud, still slow, but a little better

--- mabdi/FilterPointCloud.py
+++ mabdi/FilterPointCloud.py
@@ -32,11 +32,13 @@ class FilterPointCloud(VTKPythonAlgorithmBase):
         self._cells = []
 
         self._points = vtk.vtkPoints()
+        self._polys = vtk.vtkCellArray()
         self._polydata = vtk.vtkPolyData()
         self._polydata.SetPoints(self._points)
-        self._vgf = vtk.vtkVertexGlyphFilter()
-        DebugTimeVTKFilter(self._vgf)
-        self._vgf.SetInputData(self._polydata)
+        self._polydata.SetPolys(self._polys)
+        #self._vgf = vtk.vtkVertexGlyphFilter()
+        #DebugTimeVTKFilter(self._vgf)
+        #self._vgf.SetInputData(self._polydata)
 
     def get_valid_pts_index(self):
         tmp_valid_pts_index = self._viewport_pts[2, :] < 1.0
@@ -69,58 +71,75 @@ class FilterPointCloud(VTKPythonAlgorithmBase):
             self._world_pts = np.ones(self._viewport_pts.shape)
             # cells (list of predefined connectivity )
             nt = (2*w)*(h-1)  # number of triangles
-            self._cells = np.zeros((3, nt), dtype=np.int)
+            cells = np.zeros((3, nt), dtype=np.int)
             i = 0
             while i < (nt/2):
                 if ((i+1) % w) != 0:
-                    self._cells[:, 2*i] = (i, i+1, w+i)
-                    self._cells[:, 2*i+1] = (i+1, w+i+1, w+i)
+                    cells[:, 2*i] = (i, i+1, w+i)
+                    cells[:, 2*i+1] = (i+1, w+i+1, w+i)
                 i += 1
+            index = np.where(cells.any(axis=0))[0]
+            self._cells = cells[:, index]
 
         # update the viewport points
         self._viewport_pts[2, :] = numpy_support.vtk_to_numpy(inp.GetPointData().GetScalars())
 
         # project to world coordinates
+        tstart = timer()
         self._world_pts = np.dot(inp.tmat, self._viewport_pts)
         self._world_pts = self._world_pts / self._world_pts[3]
+        tend = timer()
+        logging.debug('Project points {:.4f} seconds'.format(tend - tstart))
 
         # update polydata
-        """
-        valid_pts_index = self._viewport_pts[2, :] < 1.0
-        vtkarray = dsa.numpyTovtkDataArray(self._world_pts[0:3, valid_pts_index].T)
-        self._points.SetData(vtkarray)
-        self._polydata.SetPoints(self._points)
-        self._vgf.Update()
-        """
-        valid_pts_index = self._viewport_pts[2, :] < 1.0
-        points = vtk.vtkPoints()
-        triangles = vtk.vtkCellArray()
+        valid_index = self._viewport_pts[2, :] < 1.0
+
+        tstart = timer()
+        # points = vtk.vtkPoints()
+        # triangles = vtk.vtkCellArray()
+        tend = timer()
+        logging.debug('Initialize vtk objects {:.4f} seconds'.format(tend - tstart))
 
         # http://stackoverflow.com/a/21448251/4068274
-        for i in list(compress(xrange(len(valid_pts_index)), valid_pts_index)):
-            points.InsertPoint(i, *self._world_pts[0:3, i])
-        logging.debug('points done')
+        tstart = timer()
+        for i in list(compress(xrange(len(valid_index)), valid_index)):
+            self._points.InsertPoint(i, *self._world_pts[0:3, i])
+        tend = timer()
+        logging.debug('Points loop {:.4f} seconds'.format(tend - tstart))
 
+        tstart = timer()
         for i in np.arange(self._cells.shape[1]):
             (tpt1, tpt2, tpt3) = self._cells[:, i]  # triangle points
-            if valid_pts_index[tpt1] and valid_pts_index[tpt2] and valid_pts_index[tpt3]:
-                triangles.InsertNextCell(3)
-                triangles.InsertCellPoint(tpt1)
-                triangles.InsertCellPoint(tpt2)
-                triangles.InsertCellPoint(tpt3)
-        logging.debug('polys done')
+            if valid_index[tpt1] and valid_index[tpt2] and valid_index[tpt3]:
+                self._polys.InsertNextCell(3)
+                self._polys.InsertCellPoint(tpt1)
+                self._polys.InsertCellPoint(tpt2)
+                self._polys.InsertCellPoint(tpt3)
+        tend = timer()
+        logging.debug('Polys loop {:.4f} seconds'.format(tend - tstart))
 
-        plt.imshow(valid_pts_index.reshape((self._sizey, self._sizex)), origin='lower')
+        plt.imshow(valid_index.reshape((self._sizey, self._sizex)), origin='lower')
         plt.show()
 
-        self._polydata.SetPoints(points)
-        self._polydata.SetPolys(triangles)
+        tstart = timer()
+        # self._polydata.SetPoints(points)
+        # self._polydata.SetPolys(triangles)
 
         out = vtk.vtkPolyData.GetData(outInfo)
         # out.ShallowCopy(self._vgf.GetOutput())
         out.ShallowCopy(self._polydata)
+        tend = timer()
+        logging.debug('The rest {:.4f} seconds'.format(tend - tstart))
 
         end = timer()
         logging.debug('Execution time {:.4f} seconds'.format(end - start))
 
         return 1
+
+"""
+valid_pts_index = self._viewport_pts[2, :] < 1.0
+vtkarray = dsa.numpyTovtkDataArray(self._world_pts[0:3, valid_pts_index].T)
+self._points.SetData(vtkarray)
+self._polydata.SetPoints(self._points)
+self._vgf.Update()
+"""
